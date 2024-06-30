4349f2560be1b0a846d75dffca92210df8f0e7d5
Joshua Flanagan <joshuaflanagan@gmail.com>
2024-06-30 18:32:05 -0500

    Provide four built in programs
    00 - increment by 1
    01 - increment by value at 0x00
    10 - fibonacci sequence
    11 - multiple value at 0x00 by value at 0x01

--- rom_programs/rom_programs.ino
+++ rom_programs/rom_programs.ino
@@ -1,5 +1,5 @@
 /**
- * This sketch programs the software ROM EEPROMs for the 8-bit breadboard computer
+ * This sketch programs the microcode EEPROMs for the 8-bit breadboard computer
  * It includes support for a flags register with carry and zero flags
  * See this video for more: https://youtu.be/Zg1NdPKoosU
  */
@@ -13,43 +13,84 @@
 #define NOP 0b0000 << 4
 #define LDA 0b0001 << 4
 #define ADD 0b0010 << 4
+#define ADI 0b1001 << 4
 #define SUB 0b0011 << 4
+#define SBI 0b1010 << 4
 #define STA 0b0100 << 4
 #define LDI 0b0101 << 4
 #define JMP 0b0110 << 4
 #define JC  0b0111 << 4
 #define JZ  0b1000 << 4
-#define ADI 0b1001 << 4
-#define SUI 0b1010 << 4
 #define OUT 0b1110 << 4
 #define HLT 0b1111 << 4
 
-// - the 4 high bytes are the operation
-// - the 4 low bytes are the argument
 
 // count by 1
+#define COUNTER 14
 byte program0[] = {
   LDI | 1,
+  STA | COUNTER,
   OUT,
-  ADI | 1,
+  ADD | COUNTER,
+  JMP | 2
+};
+
+// memory locations to use for variables
+#define X 0
+#define Y 1
+
+// count by value in 0x00
+byte program1[] = {
+  LDI | 0,
+  OUT,
+  ADD | X,
   JMP | 1
 };
 
-// count by 5
-byte program1[] = {
-  HLT
+// fibonacci
+byte program2[] = {
+  LDI | 0,
+  STA | 15, // prev = 0
+  LDI | 1,
+  STA | 14, // curr = 1
+  // LOOP
+  ADD | 15, // A = A + prev
+  JC  | 0,  // if > 255, start over
+  OUT,
+  STA | 13, // tmp = curr + prev
+  LDA | 14, // A = curr
+  STA | 15, // prev = A (curr)
+  LDA | 13, // A = tmp
+  STA | 14, // curr = A (curr + prev)
+  JMP | 4   // goto LOOP
 };
 
-// fibonacci sequence
-byte program2[] = {
-  HLT
-};
+// multiply value in 0x00 (X) by 0x01 (Y)
+// non-destructive - does not overwrite the input variables
+// requires custom SUBI (subtract immediate) command
+//  keep running total in 0x10
+//  keep constant 1 in 0xFF
+#define RESULT 2
 
 byte program3[] = {
-  HLT
+  LDI | 0,
+  STA | RESULT,  // result = 0
+  LDA | Y,
+  STA | COUNTER, // counter = y
+  // LOOP
+  SBI | 1, // A=X-1
+  JC  | 9,  // goto NOT_DONE (subtraction sets carry, unless it wraps)
+  LDA | RESULT,  // A=result
+  OUT,      // show result
+  HLT,      // exit
+  // NOT_DONE
+  STA | COUNTER,  // counter = counter - 1
+  LDA | RESULT,  // A=result
+  ADD | X,  // A=result + first factor
+  STA | RESULT,  // result = A (result + second factor)
+  LDA | COUNTER,  // load counter
+  JMP | 4,  // goto LOOP 
 };
-
-
 
 /*
  * Output the address bits and outputEnable signal using shift registers.
@@ -100,104 +141,70 @@ void writeEEPROM(int address, byte data) {
   delay(10);
 }
 
-void print16bytes(int address, byte data[16]){
-    char buf[80];
-    sprintf(buf, "%03x:  %02x %02x %02x %02x %02x %02x %02x %02x   %02x %02x %02x %02x %02x %02x %02x %02x",
-            address, data[0], data[1], data[2], data[3], data[4], data[5], data[6], data[7],
-            data[8], data[9], data[10], data[11], data[12], data[13], data[14], data[15]);
-
-    Serial.println(buf);
-}
 
 /*
  * Read the contents of the EEPROM and print them to the serial monitor.
  */
 void printContents(int start, int length) {
-  for (int base = start; base < (start + length); base += 16) {
+  for (int base = start; base < length; base += 16) {
     byte data[16];
     for (int offset = 0; offset <= 15; offset += 1) {
       data[offset] = readEEPROM(base + offset);
     }
 
-    print16bytes(base, data);
+    char buf[80];
+    sprintf(buf, "%03x:  %02x %02x %02x %02x %02x %02x %02x %02x   %02x %02x %02x %02x %02x %02x %02x %02x",
+            base, data[0], data[1], data[2], data[3], data[4], data[5], data[6], data[7],
+            data[8], data[9], data[10], data[11], data[12], data[13], data[14], data[15]);
+
+    Serial.println(buf);
   }
 }
 
 byte rom[1024] = {0};
-  
+
 void setup() {
-  Serial.begin(57600); // make sure Serial Monitor uses same baud
-  Serial.println("Press Enter to program EEPROM:");
-  while (Serial.available() < 1){
-    delay(200);
-  }
-  Serial.read();
 
   pinMode(SHIFT_DATA, OUTPUT);
   pinMode(SHIFT_CLK, OUTPUT);
   pinMode(SHIFT_LATCH, OUTPUT);
   digitalWrite(WRITE_EN, HIGH);
   pinMode(WRITE_EN, OUTPUT);
+  Serial.begin(57600);
 
   // Program data bytes
   Serial.print("Programming EEPROM");
 
-  // There are 11 address pins
-  // The most significant bit (A10) toggles between regular output or inverted output
-  // The next two lower bits (A8-A9) determine which program is running (0 - 3)
-  // The other 8 bits determining the step in the program. For now, since our program
-  // counter is limited to 4 bits, we will only use the lowest 4 bits (A0-A3). 
-  byte progID;
-
-  // I=inverted, PP=progID, xxxx=ignored, ssss=step
-  // I PP xxxx ssss
-
-  // Copy the programs into the 0-initialized rom variable
-  
-  progID = 0b00;
-  for(int i=0; i<sizeof(program0); i++){
-    rom[(progID << 8) + i] = program0[i];
+  byte progID = 0b00;
+  for (int i=0;i<sizeof(program0); i++){
+    rom[ (progID << 8) + i] = program0[i];
   }
-
-  progID = 0b01;
-  for(int i=0; i<sizeof(program1); i++){
-    rom[(progID << 8) + i] = program1[i];
+   progID = 0b01;
+  for (int i=0;i<sizeof(program1); i++){
+    rom[ (progID << 8) + i] = program1[i];
   }
-
   progID = 0b10;
-  for(int i=0; i<sizeof(program2); i++){
-    rom[(progID << 8) + i] = program2[i];
+  for (int i=0;i<sizeof(program2); i++){
+    rom[ (progID << 8) + i] = program2[i];
   }
-
   progID = 0b11;
-  for(int i=0; i<sizeof(program3); i++){
-    rom[(progID << 8) + i] = program3[i];
+  for (int i=0;i<sizeof(program3); i++){
+    rom[ (progID << 8) + i] = program3[i];
   }
 
-
-  Serial.println();
-
-
-  // Write out the programs, in the lower 1024 bytes (A10=0)
-  for (int address = 0; address < 1024; address += 1){
+  // Program the 8 high-order bits of microcode into the first 128 bytes of EEPROM
+  for (int address = 0; address < 1024; address += 1) {
+    // write rom here
     writeEEPROM(address, rom[address]);
     
     if (address % 64 == 0) {
       Serial.print(".");
     }
   }
-
-  // Write out the inverted bits, in the upper 1024 bytes (A10=1)
-  for (int address = 0; address < 1024; address += 1){
-    writeEEPROM(address + 1024, ~rom[address]);
-
-    if (address % 64 == 0) {
-      Serial.print(".");
-    }
-  }
-
   Serial.println(" done");
 
+
+  // Read and print out the contents of the EERPROM
   Serial.println("Reading EEPROM");
   printContents(0, 2048);
 }
